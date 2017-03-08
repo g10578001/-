cf2980c074495bbc0b0359f1e1fd146b74f04f5e
Liang <g10578001@cycu.edu.tw>
2017-03-08 14:43:47 +0800

    BEGIN006解答上傳(cpp, word, pdf)

---
+++ CPE顆星廣場/CPE輸出入/BEGIN006/BEGIN006.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	int ans=0, x;
+	while(getline(cin, line))
+	{
+		istringstream s(line);
+		while(s>>x)ans+=x;
+		printf("Sum=%d\n", ans);
+		ans=0;
+	}
+	return 0;
+}
